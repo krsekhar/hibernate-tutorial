e0afdc08120d74e96139eae985d0c81bc6a4b7a4
choco.chetu <choco.chetu@b98a2591-6706-5edc-5f31-030cb1b70918>
2010-05-18 07:50:27 +0000

    STABLE: Working the association

--- trunk/src/main/resources/SQLScripts/schema.sql
+++ trunk/src/main/resources/SQLScripts/schema.sql
@@ -1,2 +1,4 @@
 create database hibernateDB;
 create table events(EVENT_ID int auto_increment primary key,EVENT_DATE timestamp,title varchar(50));
+create table Person(PERSON_ID int auto_increment primary key,age int,firstname varchar(30),lastname varchar(30));
+create table PERSON_EVENT(PERSON_ID int,EVENT_ID int, primary key(PERSON_ID,EVENT_ID));
